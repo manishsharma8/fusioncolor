963987180e26cfe3250aec4fe93c0f9feb457f19
Manish Sharma <sharmamanish8301@gmail.com>
2022-05-27 12:27:51 +0530

    feat: TailwindCSS Copy to Dashboard

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,6 @@
+import { useEffect, useState } from 'react';
 import type { NextPage } from 'next';
 import { directions } from '../utils/directions';
-import { useState } from 'react';
 
 interface CardProps {
 	name: string;
@@ -8,18 +8,46 @@ interface CardProps {
 }
 
 const Card: NextPage<CardProps> = ({ name, colors }) => {
-	const [direction, setDirection] = useState('bg-gradient-to-t');
+	const [direction, setDirection] = useState<string>('bg-gradient-to-t');
+	const [gradient, setGradient] = useState<string>(
+		'bg-gradient-to-t ' + colors
+	);
+
+	useEffect(() => {
+		setGradient(direction + ' ' + colors);
+	}, [direction, colors]);
 
 	return (
-		<div key={name}>
+		<div>
 			<div className={`${direction} ${colors} h-64 w-full rounded-3xl`}></div>
 			<div className="-mt-12 mx-3 p-6 h-20 bg-gray-900 rounded-3xl shadow-lg">
-				<div className="text-xl font-bold">{name.toUpperCase()}</div>
-				<div className="mt-5 grid grid-cols-8 gap-1.5">
+				<div className="flex justify-between">
+					<div className="text-xl font-bold">{name.toUpperCase()}</div>
+					<button
+						className="w-9 h-9 bg-gray-800 p-1 rounded-lg hover:bg-rose-600 transition-colors duration-300 ease-in"
+						onClick={() => navigator.clipboard.writeText(gradient)}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="mx-auto h-5 w-5 text-gray-100"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+							strokeWidth={2}
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
+							/>
+						</svg>
+					</button>
+				</div>
+				<div className="mt-5 grid grid-cols-8 gap-0.5">
 					{directions.map((dir) => (
 						<div
 							key={dir.key}
-							className="bg-gray-800 h-8 w-8 rounded-lg p-1.5 hover:bg-rose-600 transition-colors duration-300 ease-in cursor-pointer"
+							className="bg-gray-800 h-9 w-9 rounded-lg p-2 hover:bg-rose-600 transition-colors duration-300 ease-in cursor-pointer"
 							onClick={() => setDirection(dir.gradient)}
 						>
 							<svg
